3cd45df1a138204e47d27ae38a706927c942315e
Henrique de Mello Franco <henrique.mf@gmail.com>
2023-09-05 14:58:40 -0300

    products and categories controller initial methods

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,19 @@
 class CategoriesController < ApplicationController
+  def index
+    @categories = Category.all
+  end
+
+  def show
+    @category = Category.find(params[:id])
+  end
+
+  # def search
+  #   @search_results = Category.where("name ILIKE ?", "%#{params[:search][:query]}%")
+  # end
+
+  private
+
+  def set_category
+    @category = Category.find(params[:id])
+  end
 end
