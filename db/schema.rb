3e0fc3384ca35e7fe06b92cabda60fc4d7aeb5e2
andre kim <andrekim640@gmail.com>
2023-09-06 17:23:28 -0300

    index

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_09_06_144856) do
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -126,11 +125,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_06_144856) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-
-  add_foreign_key "carts", "products"
-  add_foreign_key "carts", "users"
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "carts", "products"
+  add_foreign_key "carts", "users"
   add_foreign_key "grades", "schools"
   add_foreign_key "lists", "grades"
   add_foreign_key "order_products", "orders"
