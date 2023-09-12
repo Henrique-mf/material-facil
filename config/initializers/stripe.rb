251f37686f4c5ce54ad9c98f619df12ef5935ae2
Henrique de Mello Franco <henrique.mf@gmail.com>
2023-09-12 18:54:48 -0300

    step 1

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -7,6 +7,6 @@ Rails.configuration.stripe = {
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
 StripeEvent.signing_secret = Rails.configuration.stripe[:signing_secret]
 
-StripeEvent.configure do |events|
-  events.subscribe 'checkout.session.completed', StripeCheckoutSessionService.new
-end
+# StripeEvent.configure do |events|
+#   events.subscribe 'checkout.session.completed', StripeCheckoutSessionService.new
+# end
